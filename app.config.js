713b4f39bc0d80fb8d1a0a5bf3ca8efc046373a7
Abdullah <shinsukenakamurahard@gmail.com>
2025-08-06 11:11:17 +0500

    Expo configurations

---
+++ app.config.js
@@ -0,0 +1,38 @@
+import 'dotenv/config';
+
+export default {
+  expo: {
+    name: "movie",
+    slug: "movie",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    newArchEnabled: true,
+    splash: {
+      image: "./assets/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    ios: {
+      supportsTablet: true
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      },
+      edgeToEdgeEnabled: true,
+      package: "com.abdullahazmat.movie"
+    },
+    web: {
+      favicon: "./assets/favicon.png"
+    },
+    extra: {
+      eas: {
+        projectId: "f0be34f6-bd23-4068-93c3-52d5e6fb000b"
+      },
+      MOVIE_API_KEY: process.env.MOVIE_API_KEY,
+    }
+  }
+};
